44122f94e85dc5ef81a99267c2206602910485f8
Kauã Régis <kauarlc@hotmail.com>
2025-05-22 11:19:34 -0300

    fix: campo descrição pode ser nulo

--- api_drf/api/models.py
+++ api_drf/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Task(models.Model): # cria um modelo de tarefa
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=60)
-    description = models.TextField(max_length=250)
+    description = models.TextField(max_length=250, blank=True)
     done = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
